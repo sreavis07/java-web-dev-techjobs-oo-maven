1191386982dde5ee8300e390c4c60511a187a2e6
sreavis07 <sean_reavis@cable.comcast.com>
2021-07-09 11:48:00 -0400

    3rd Commit

--- src/main/java/com/company/PositionType.java
+++ src/main/java/com/company/PositionType.java
@@ -2,48 +2,47 @@ package com.company;
 
 import java.util.Objects;
 
-public class PositionType {
-    private static int nextId = 1;
-
-    private int id;
-    private String value;
-
-    public PositionType() {
-        id = nextId;
-        nextId++;
-    }
+public class PositionType extends JobField{
+//    private static int nextId = 1;
+//
+//    private int id;
+//    private String value;
+//
+//    public PositionType() {
+//        id = nextId;
+//        nextId++;
+//    }
 
     public PositionType(String value) {
-        this();
-        this.value = value;
+        super(value);
     }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
 
-    @Override
-    public String toString() {
-        return this.value;
-    }
-
-    // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when their id fields match.
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        PositionType position = (PositionType) obj;
-        return id == position.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-    // Getters and Setters:
-
-    public int getId() { return this.id; };
-
-    public String getValue() { return this.value; }
-
-    public void setValue(String value) { this.value = value; }
+//    @Override
+//    public String toString() {
+//        return this.value;
+//    }
+//
+//    // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when their id fields match.
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (this == obj) return true;
+//        if (obj == null || getClass() != obj.getClass()) return false;
+//        PositionType position = (PositionType) obj;
+//        return id == position.id;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(id);
+//    }
+//    // Getters and Setters:
+//
+//    public int getId() { return this.id; };
+//
+//    public String getValue() { return this.value; }
+//
+//    public void setValue(String value) { this.value = value; }
 }
